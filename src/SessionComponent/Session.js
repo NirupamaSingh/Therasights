4748d27f9d9493b7b1cc92dc25c8c62891e846fd
Nirupama Singh <nirupama15singh@gmail.com>
2021-09-01 08:14:22 +0530

    session

--- src/SessionComponent/Session.js
+++ src/SessionComponent/Session.js
@@ -1,14 +1,11 @@
 import React from "react";
 import './session.css';
-import { Link } from 'react-router-dom';
 
-const Session = () => {
+const Session = () => { 
 
     return (
         <>
-            <Link to="/mseForm">
-                <button className="endButton">End Session</button>
-            </Link>
+            
         </>
     );
 };
